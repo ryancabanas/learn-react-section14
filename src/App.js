5eace17c1eb9b9b3e768e63766543942a64346d7
Ryan Cabanas <ryangithub777@gmail.com>
2022-07-16 22:30:07 -0400

    End of Lesson 180: Use and error state, a try/catch, and some conditional output depending on states.
    - Add a simple error state that is set up with a try/catch block to respond to
      HTTP error responses.  We're using `fetch`, so it kind of sucks.  Our error
      handling sucks and is a lot more work.  Use `axios` for the future because
      it will provide legit HTTP error response info we can work with.
    
    - Clean up how the conditional response is handled.  Use a single `content`
      variable and then change it's content based on the state of the application.

--- src/App.js
+++ src/App.js
@@ -6,31 +6,48 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchMoviesHandler = async () => {
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films');
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => ({
-      id: movieData.episode_id,
-      title: movieData.title,
-      releaseDate: movieData.release_date,
-      openingText: movieData.opening_crawl,
-    }));
-    setMovies(transformedMovies);
+    setError(null);
+
+    try {
+      const response = await fetch('https://swapi.dev/api/films');
+      if (!response.ok) {
+        throw new Error('Something went wrong.');
+      }
+
+      const data = await response.json();
+      const transformedMovies = data.results.map((movieData) => ({
+        id: movieData.episode_id,
+        title: movieData.title,
+        releaseDate: movieData.release_date,
+        openingText: movieData.opening_crawl,
+      }));
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   };
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  } else if (error) {
+    content = <p>{error}</p>;
+  } else if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
