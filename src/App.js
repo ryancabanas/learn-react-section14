53a248dc9cddea0d843c32382e73cfc38d19bb5c
Ryan Cabanas <ryangithub777@gmail.com>
2022-07-16 21:56:58 -0400

    End of Lesson 178: Convert Promise syntax to async/await syntax.

--- src/App.js
+++ src/App.js
@@ -6,32 +6,32 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  // const fetchMoviesHandler = async () => {
-  //   const response = await fetch('https://swapi.dev/api/films');
-  //   const data = await response.json();
-  //   const transformedMovies = data.results.map((movieData) => ({
-  //     id: movieData.episode_id,
-  //     title: movieData.title,
-  //     releaseDate: movieData.release_date,
-  //     openingText: movieData.opening_crawl,
-  //   }));
-  //   setMovies(transformedMovies);
-  // };
-
-  const fetchMoviesHandler = () => {
-    fetch('https://swapi.dev/api/films')
-      .then((response) => response.json())
-      .then((data) =>
-        setMovies(
-          data.results.map((movieData) => ({
-            id: movieData.episode_id,
-            title: movieData.title,
-            releaseDate: movieData.release_date,
-            openingText: movieData.opening_crawl,
-          }))
-        )
-      );
+  const fetchMoviesHandler = async () => {
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
+    const transformedMovies = data.results.map((movieData) => ({
+      id: movieData.episode_id,
+      title: movieData.title,
+      releaseDate: movieData.release_date,
+      openingText: movieData.opening_crawl,
+    }));
+    setMovies(transformedMovies);
   };
+
+  // const fetchMoviesHandler = () => {
+  //   fetch('https://swapi.dev/api/films')
+  //     .then((response) => response.json())
+  //     .then((data) =>
+  //       setMovies(
+  //         data.results.map((movieData) => ({
+  //           id: movieData.episode_id,
+  //           title: movieData.title,
+  //           releaseDate: movieData.release_date,
+  //           openingText: movieData.opening_crawl,
+  //         }))
+  //       )
+  //     );
+  // };
 
   return (
     <React.Fragment>
