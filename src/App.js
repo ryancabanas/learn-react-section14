9af773b79ec2ad7a4363fc616e55d9b30f91be04
Ryan Cabanas <ryangithub777@gmail.com>
2022-07-16 22:04:34 -0400

    End of Lesson 179: Set up some simple conditionals on screen depending on state.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMoviesHandler = async () => {
+    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films');
     const data = await response.json();
     const transformedMovies = data.results.map((movieData) => ({
@@ -16,22 +18,8 @@ function App() {
       openingText: movieData.opening_crawl,
     }));
     setMovies(transformedMovies);
+    setIsLoading(false);
   };
-
-  // const fetchMoviesHandler = () => {
-  //   fetch('https://swapi.dev/api/films')
-  //     .then((response) => response.json())
-  //     .then((data) =>
-  //       setMovies(
-  //         data.results.map((movieData) => ({
-  //           id: movieData.episode_id,
-  //           title: movieData.title,
-  //           releaseDate: movieData.release_date,
-  //           openingText: movieData.opening_crawl,
-  //         }))
-  //       )
-  //     );
-  // };
 
   return (
     <React.Fragment>
@@ -39,7 +27,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
