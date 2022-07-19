d27adf00b877f5bec3ad51e4817b8dc8d718a66d
Ryan Cabanas <ryangithub777@gmail.com>
2022-07-18 21:46:34 -0400

    End of Lesson 183:  Implement a POST request.
    - Add the `AddMovie` component which allows us to make a POST request to add
      movies to a Firebase (Google) API.
    
    - Implement the `AddMovie` component into the `App` component and pass it the
      `addMovieHandler` function that contains the POST.
    
    - Amend the `fetchMoviesHandler` function to account for the format of the data
      returned by the Firebase API, which is not an array, but everything wrapped
      in an object.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -8,23 +9,32 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  const url =
+    'https://react-http-4bc80-default-rtdb.firebaseio.com/movies.json';
+
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-
     try {
-      const response = await fetch('https://swapi.dev/api/films');
+      const response = await fetch(url);
       if (!response.ok) {
-        throw new Error('Something went wrong.');
+        throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
-      const transformedMovies = data.results.map((movieData) => ({
-        id: movieData.episode_id,
-        title: movieData.title,
-        releaseDate: movieData.release_date,
-        openingText: movieData.opening_crawl,
-      }));
+
+      const transformedMovies = [];
+      for (const key in data) {
+        if (Object.hasOwnProperty.call(data, key)) {
+          const element = data[key];
+          transformedMovies.push({
+            id: key,
+            title: element.title,
+            openingText: element.openingText,
+            releaseDate: element.releaseDate,
+          });
+        }
+      }
       setMovies(transformedMovies);
     } catch (error) {
       setError(error.message);
@@ -35,6 +45,16 @@ function App() {
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 
   let content = <p>Found no movies.</p>;
 
@@ -52,6 +72,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
